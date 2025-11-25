0330b3e8c10f32b42c6af0cd55b14cf4e99e9c9e
LuizEduardoRezende <luiz.rezendemoraes@gmail.com>
2025-11-25 01:08:05 -0300

    update firmware spam_detector

--- test/tflm-tests/comparacao-firmwares/firmware_spam_detector.cc
+++ test/tflm-tests/comparacao-firmwares/firmware_spam_detector.cc
@@ -3,8 +3,6 @@
 #include "tensorflow/lite/micro/micro_interpreter.h"
 #include "tensorflow/lite/micro/micro_log.h"
 #include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
-#include "tensorflow/lite/micro/micro_profiler.h"
-#include "tensorflow/lite/micro/recording_micro_interpreter.h"
 #include "tensorflow/lite/micro/system_setup.h"
 #include "tensorflow/lite/schema/schema_generated.h"
 
